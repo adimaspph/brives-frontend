f511e935b647c76ffe25f4c4cf5c9572fb02fd16
fadiya.latifah <fadiya.latifah@ui.ac.id>
2022-03-16 10:32:17 +0700

    PBI 9 Add Form CSS

--- src/services/MapelService.js
+++ src/services/MapelService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 
 const MAPEL_API_BASE_URL = "http://localhost:8080/mapel/";
+const JENJANG_API_BASE_URL = "http://localhost:8080/jenjang/";
+
 class MapelService {
     getMapel(){
         return axios.get(MAPEL_API_BASE_URL);
@@ -9,6 +11,10 @@ class MapelService {
 
     createMapel(mapel){
         return axios.post(MAPEL_API_BASE_URL, mapel);
+    }
+
+    getJenjang(){
+        return axios.get(JENJANG_API_BASE_URL);
     }
 
 }
