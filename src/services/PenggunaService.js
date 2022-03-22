d82d514b63ab614e649e061c7250e2a91b651490
fadiya.latifah <fadiya.latifah@ui.ac.id>
2022-03-22 23:20:33 +0700

    PBI 9 & 11 - add auth

--- src/services/PenggunaService.js
+++ src/services/PenggunaService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import APIConfig from '../api/APIConfig';
 
 
-const PENGGUNA_API_BASE_URL = "http://localhost:8080/api/v1/user/all";
-const ROLE_API_BASE_URL = "http://localhost:8080/role";
-const IDPENGGUNA_API_BASE_URL = "http://localhost:8080/api/v1/user";
+// const PENGGUNA_API_BASE_URL = "http://localhost:8080/api/v1/user/all";
+// const ROLE_API_BASE_URL = "http://localhost:8080/role";
+// const IDPENGGUNA_API_BASE_URL = "http://localhost:8080/api/v1/user";
 
 // const PENGGUNA_API_BASE_URL = "https://brives-staging-backend.herokuapp.com/api/v1/user/all";
 // const ROLE_API_BASE_URL = "https://brives-staging-backend.herokuapp.com/role";
