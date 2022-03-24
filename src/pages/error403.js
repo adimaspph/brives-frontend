5fa9c9ae903c1b54aa0ecbee04871ecd92f68b27
fadiya.latifah <fadiya.latifah@ui.ac.id>
2022-03-24 18:46:04 +0700

    styling 403 page

--- src/pages/error403.js
+++ src/pages/error403.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class error403 extends React.Component {
 	constructor(props) {
@@ -9,12 +10,22 @@ class error403 extends React.Component {
 
 	render() {
 		return (
-			<div>
+			<div className="box-403">
 				<a href="" class="fa fa-arrow-left"></a>
 				<div class="error403">
-					<h1>Forbidden 403</h1>
-					<p>Anda tidak memiliki akses untuk melihat halaman ini.</p>
+					<h1 className="text-403">Forbidden 403</h1>
+					<p className="text-small-403">Anda tidak memiliki akses untuk melihat halaman ini.</p>
+
+					<div className='centerin'>
+						<Link className="btn btn-blue twobutton" to="/">
+							Kembali Ke Beranda
+						</Link>
+
+					</div>
+
 				</div>
+
+
 			</div>
 		);
 	}
