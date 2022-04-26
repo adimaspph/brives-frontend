24f3b080b66d9c491d97b55dae38ebb48103cb11
adimaspph <adimas0101@gmail.com>
2022-04-27 01:26:18 +0700

    make private booking page

--- src/pages/PesanKelasPage/PesanKelasPage.js
+++ src/pages/PesanKelasPage/PesanKelasPage.js
@@ -1,18 +1,102 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar';
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./PesanKelasPage.css";
+import APIConfig from "../../api/APIConfig";
 
 export default function PesanKelasPage() {
-    return (
+	const [listJenjang, setListJenjang] = useState([]);
+	const [selectedJenjang, setSelectedJenjang] = useState("all");
+	const [listMapel, setListMapel] = useState([]);
+	
+	const setJejang = (idJenjang) => {
+		setSelectedJenjang(idJenjang);
+
+		// console.log(selectedJenjang + idJenjang.toString());
+		getMapel(idJenjang);
+	};
+
+	const getMapel = (idJenjang) => {
+		APIConfig.get("/mapel/jenjang/" + idJenjang)
+			.then((response) => {
+				console.log(response.data.result);
+				setListMapel(response.data.result);
+			})
+			.catch((error) => {
+				console.log(error.response);
+			});
+	};
+
+	const getJenjang = () => {
+		APIConfig.get("/jenjang/")
+			.then((response) => {
+				// console.log(response.data.result);
+				setListJenjang(response.data.result);
+			})
+			.catch((error) => {
+				console.log(error.response);
+			});
+	};
+
+	useEffect(() => {
+		getJenjang();
+	}, []);
+
+
+	return (
 		<div>
 			<Navbar />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<h1 className="text-center">Pilih Mata Pelajaran</h1>
-			<h4 className="text-center">Test</h4>
+
+			<h1 className="text-center title">Pilih Mata Pelajaran</h1>
+
+			<div className="jenjang-select">
+				<span
+					className={
+						selectedJenjang === "all"
+							? "jenjang-btn jenjang-selected"
+							: "jenjang-btn"
+					}
+					onClick={() => setJejang("all")}
+				>
+					Semua
+				</span>
+				{listJenjang.map((jenjang, key) => (
+					<span
+						key={key}
+						className={
+							selectedJenjang === jenjang.idJenjang
+								? "jenjang-btn jenjang-selected"
+								: "jenjang-btn"
+						}
+						onClick={() => setJejang(jenjang.idJenjang)}
+					>
+						{jenjang.namaJenjang}
+					</span>
+				))}
+			</div>
+
+			<div className="container-mapel">
+				{listMapel.map((mapel, key) => (
+					<div className="mapel-card">
+						<span className="title-mapel">{mapel.namaMapel}</span>
+
+						<div className="jenjang-mapel">
+							{mapel.listJenjang.map((jenjang, key) => (
+								<span>{jenjang.namaJenjang}</span>
+							))}
+						</div>
+						<a className="btn btn-primary btn-mapel">Pilih</a>
+					</div>
+				))}
+				{/* <div className="mapel-card">
+					<span className="title-mapel">Nama Mapel</span>
+					<div className="jenjang-mapel">
+						<span>Kelas 10</span>
+						<span>Kelas 11</span>
+						<span>Kelas 12</span>
+					</div>
+					<a className="btn btn-primary btn-mapel">Pilih</a>
+				</div> */}
+			</div>
 		</div>
 	);
 }
